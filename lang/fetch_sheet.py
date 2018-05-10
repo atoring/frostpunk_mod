d84aff74c7c5c0ae33730f65c8da52f852c9ec07
atoring <tori@air.ocn.ne.jp>
2018-05-11 07:03:27 +0900

    update: save UTF8 w/BOM

--- lang/fetch_sheet.py
+++ lang/fetch_sheet.py
@@ -4,11 +4,26 @@
 # 翻訳シートをwebから取得して./data/Frostpunk 翻訳作業所 - 翻訳.csvに保存します。
 # (手動で取得して保存する事と同等です。)
 
+import codecs
 import urllib.request
 
-url = "https://docs.google.com/spreadsheets/d/1-eu8GT6_zI4IOTHWFymplV81GJj1Q469FSWv6jGUHH8/export?format=csv&gid=2068465123"
-path = "./data/Frostpunk 翻訳作業所 - 翻訳.csv"
+def write_txt(path, data):
+    print("write file: %s" % path)
+    try:
+        f = codecs.open(path, "w", "utf_8_sig") # with bom
+    except IOError:
+        print("error: file open error: %s" % path)
+        quit()
+    f.write(data)
+    f.close()
+    print("write size: %xh" % len(data))
 
-print("fetch url: %s" % url)
-urllib.request.urlretrieve(url, path)
-print("write file: %s" % path)
+def fetch_sheet():
+    url = "https://docs.google.com/spreadsheets/d/1-eu8GT6_zI4IOTHWFymplV81GJj1Q469FSWv6jGUHH8/export?format=csv&gid=2068465123"
+    path = "./data/Frostpunk 翻訳作業所 - 翻訳.csv"
+
+    print("fetch url: %s" % url)
+    data = urllib.request.urlopen(url).read().decode("utf_8")
+    write_txt(path, data)
+
+fetch_sheet()
