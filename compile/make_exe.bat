9ae73938ccd9f1a2887ab78f82758bcf26f7985f
atoring <tori@air.ocn.ne.jp>
2018-05-13 03:53:54 +0900

    update: compile script

---
+++ compile/make_exe.bat
@@ -0,0 +1,31 @@
+@echo off
+
+set pyi="C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\Scripts\pyinstaller.exe"
+
+for %%f in (..\dat\*.py) do %pyi% --onefile %%f
+copy dist\*.exe ..\dat
+rd /S /Q ..\dat\__pycache__
+rd /S /Q build
+rd /S /Q dist
+del /Q *.spec
+
+for %%f in (..\binfont\*.py) do %pyi% --onefile %%f
+copy dist\*.exe ..\binfont
+rd /S /Q ..\binfont\__pycache__
+rd /S /Q build
+rd /S /Q dist
+del /Q *.spec
+
+for %%f in (..\lang\*.py) do %pyi% --onefile %%f
+copy dist\*.exe ..\lang
+rd /S /Q ..\lang\__pycache__
+rd /S /Q build
+rd /S /Q dist
+del /Q *.spec
+
+for %%f in (..\general\*.py) do %pyi% --onefile --windowed %%f
+copy dist\*.exe ..\general
+rd /S /Q ..\general\__pycache__
+rd /S /Q build
+rd /S /Q dist
+del /Q *.spec
