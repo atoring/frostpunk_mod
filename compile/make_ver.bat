b77061597bcc9ba51af025b92952091cebad57fd
atoring <tori@air.ocn.ne.jp>
2018-05-15 17:51:54 +0900

    fix: obtaining version

--- compile/make_ver.bat
+++ compile/make_ver.bat
@@ -9,4 +9,4 @@ set t=%t:~0,5%
 set t=%t::=%
 rem echo version_str = "%d%.%t%">%bat%
 
-%git% show --format=version_str=\"%%cd @%%h\" --date=format:%%Y%%m%%d.%%H%%M -s>%bat%
+%git% show --format="version_str=\"%%cd @%%h\"" --date=format:%%Y%%m%%d.%%H%%M -s>%bat%
