eb419d0efd1336d82ed1f8cf96d29711b671cc81
atoring <tori@air.ocn.ne.jp>
2018-05-15 17:33:14 +0900

    change method of acquiring version

--- compile/make_ver.bat
+++ compile/make_ver.bat
@@ -1,10 +1,12 @@
 @echo off
 
+set git="C:\Program Files\Git\bin\git.exe"
 set bat="..\general\version.py"
 
 set d=%date:/=%
 set t=%time: =0%
 set t=%t:~0,5%
 set t=%t::=%
+rem echo version_str = "%d%.%t%">%bat%
 
-echo version_str = "%d%.%t%">%bat%
+%git% show --format=version_str=\"%%cd @%%h\" --date=format:%%Y%%m%%d.%%H%%M -s>%bat%
